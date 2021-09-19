c2c1024bfa7e21f8f001cd27ba7a7f045bbca192
ssosnovenko <Pale1956we>
2021-09-19 14:32:14 +0300

    Initialize application

---
+++ src/main/java/com/example/baeldungcicdprocess/CiCdApplication.java
@@ -0,0 +1,11 @@
+package com.example.baeldungcicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdApplication.class, args);
+    }
+}
